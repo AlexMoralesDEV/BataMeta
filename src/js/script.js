b042d0fd6b0e4c4d2d8d453e9ff0263d8e136acb
Alex <dev.alexmorales@gmail.com>
2023-11-28 19:10:58 -0300

    Função Registrar Adicionada

--- src/js/script.js
+++ src/js/script.js
@@ -5,43 +5,90 @@ class Usuario {
         this.metas = [];
     }
 
-    static validarUsuario() {
-        // Implementação da validação do usuário
-    }
-
     static cadastrarUsuario(usuario, senha) {
-        let novoUsuario = new Usuario(usuario, senha);
-        usuarios.push(novoUsuario);
-        return novoUsuario;
+        if (Usuario.validarUsuarios()) {
+            let novoUsuario = new Usuario(usuario, senha);
+            usuarios.push(novoUsuario);
+            console.log(usuarios);
+            Usuario.limparDados();
+            return novoUsuario;
+        }
     }
 
-    adicionarMeta(nome, valorMeta, dataMeta, valorAtual){
+    static validarUsuarios() {
+        let flag = true;
+        let inputs = document.querySelectorAll('.validar');
+        let erros = document.querySelectorAll('.texto-erro');
+
+        erros.forEach((elemento) => {
+            elemento.remove();
+        })
+
+        let criaErro = function (elemento, msg) {
+            let div = document.createElement('div');
+            div.innerHTML = msg;
+            div.setAttribute('class', 'texto-erro')
+            elemento.insertAdjacentElement('afterend', div);
+        };
+
+        inputs.forEach((elemento) => {
+            let elementoAnterior = elemento.previousElementSibling.innerText;
+           
+            if (!elemento.value) {
+                criaErro(elemento, `O campo ${elementoAnterior} não pode estar vazio!`)
+                flag = false;
+            }
+
+            if(elemento.classList.contains('senha')){
+                if(elemento.value.length < 6){
+                    criaErro(elemento, `A senha deve conter no mínimo 6 caracteres`);
+                    flag = false;
+                }
+            }
+        })
+
+        return flag;
+    }
+
+    static limparDados(){
+        let inputs = document.querySelectorAll('.validar');
+
+        inputs.forEach((elemento) => {
+           elemento.value = '';
+        })
+    }
+
+    adicionarMeta(nome, valorMeta, dataMeta, valorAtual) {
         let novaMeta = new Meta(nome, valorMeta, dataMeta, valorAtual);
         this.metas.push(novaMeta);
     }
 }
 
-class Meta{
-    constructor(nome, valorMeta, dataPrazo, valorAtual = 0){
+class Meta {
+    constructor(nome, valorMeta, dataPrazo, valorAtual = 0) {
         this.nome = nome,
-        this.valorMeta = valorMeta,
-        this.dataPrazo = dataPrazo,
-        this.valorAtual = valorAtual
+            this.valorMeta = valorMeta,
+            this.dataPrazo = dataPrazo,
+            this.valorAtual = valorAtual
     }
 
-    depositarValor(valor){
+    depositarValor(valor) {
         this.valorAtual += valor;
     }
 
-    retirarValor(valor){
+    retirarValor(valor) {
         this.valorAtual -= valor;
     }
 }
 
-let usuarios = []; 
+let usuarios = [];
+let nomeUsuario = document.querySelector('#usuario');
+let senhaUsuario = document.querySelector('#senha');
+let botaoEntrar = document.querySelector('#entrar');
+let botaoRegistrar = document.querySelector('#registrar');
 
-Usuario.cadastrarUsuario('Cauã', '123');
-usuarios[0].adicionarMeta('Reserva de Emergência', 5000, '05-10-2024')
-usuarios[0].adicionarMeta('Viagem Final de Ano', 7460, '26-09-2025')
-usuarios[0].metas[0].depositarValor(500);
-console.log(usuarios[0].metas);
+function cadastrarNovoUsuario() {
+    Usuario.cadastrarUsuario(nomeUsuario.value, senhaUsuario.value);
+}
+
+botaoRegistrar.addEventListener('click', cadastrarNovoUsuario);
